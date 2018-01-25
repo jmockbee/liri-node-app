8f1bb51b1f7f30a39649df0fcd2d53757a72176d
Jon <mockbee1000@gmail.com>
2018-01-25 11:37:13 -0500

    cleaning up code for twitter and spotify

--- liri.js
+++ liri.js
@@ -1,3 +1,13 @@
+
+
+//dependencies 
+
+// Import the request npm package.
+var request = require("request");
+// Import the FS package for read/write.
+var fs = require("fs");
+
+
 let twitterKeys = require("./keys.js");
 const Twitter = require('twitter');
 
@@ -7,80 +17,56 @@ const SpotifyWebApi = require('spotify-web-api-node');
 
 
 
-
-let arg0 = process.argv[0];
-let arg1 = process.argv[1];
-let arg2 = process.argv[2];
-let arg3 = process.argv[3];
-
-
-
-
-// capture user args and call specific function
-if (arg2 === "my-tweets") {
-    getTweets();
-
-} else if (arg2 === "spotify-this-song") {
-    getSongs(arg3);
-
-} else if (arg2 === "movie-this") {
-    getMovies(arg3);
-
-} else if (arg2 === "do-what-it-says") {
-    doSomething();
-} else {
-    console.log(" not a valid input \n\n valid commands are: \n 1. node liri.js my-tweets \n 2. node liri.js spotify-this-song");
-}
-
-
-
-
-
-
-function getTweets() {
-    var client = new Twitter(twitterKeys);
-
+// Writes to the log.txt file
+var getArtistNames = function(artist) {
+    return artist.name;
+  };
+  // Function for running a Spotify search
+  var getMeSpotify = function(songName) {
+    if (songName === undefined) {
+      songName = "What's my age again";
+    }
+    spotify.search(
+      {
+        type: "track",
+        query: songName
+      },
+      function(err, data) {
+        if (err) {
+          console.log("Error occurred: " + err);
+          return;
+        }
+        var songs = data.tracks.items;
+        for (var i = 0; i < songs.length; i++) {
+          console.log(i);
+          console.log("artist(s): " + songs[i].artists.map(getArtistNames));
+          console.log("song name: " + songs[i].name);
+          console.log("preview song: " + songs[i].preview_url);
+          console.log("album: " + songs[i].album.name);
+          console.log("-----------------------------------");
+        }
+      }
+    );
+  };
+  // Function for running a Twitter Search
+  var getMyTweets = function() {
+    var client = new Twitter(keys.twitter);
     var params = {
-        screen_name: '@jonmockbee'
+      screen_name: "cnn"
     };
-    client.get('statuses/user_timeline', params, function (error, tweets, response) {
-        if (!error) {
-         
-            tweets.forEach(function (value, index) {
-                console.log(` TWEET: ${value.text} `);
-                console.log("----------------------------------------------------------------------\n");
-            });
-
+    client.get("statuses/user_timeline", params, function(error, tweets, response) {
+      if (!error) {
+        for (var i = 0; i < tweets.length; i++) {
+          console.log(tweets[i].created_at);
+          console.log("");
+          console.log(tweets[i].text);
         }
-        if (error) {
-            console.log(error);
-        }
+      }
     });
-}
+  };
+
+// add movies to this at a later point maybe??
 
 
 
 
-
-
-
-
-
-function getSongs(song) {
-    console.log(song);
-  
-
-
-}
-
-function getMovies() {
-    console.log("movie function");
-
-
-}
-// remember this 
-//function doSomething() {
- //   console.log("do what it says function");
-
-
-//}
